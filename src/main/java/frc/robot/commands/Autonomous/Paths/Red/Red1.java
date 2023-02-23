fa875ee83d61849e8baffd839878cd183e590fe6
Due Westerners Programming <frc-programmers@duewestrobotics.org>
2023-02-22 21:06:07 -0500

    Added some more auto stuff

--- src/main/java/frc/robot/commands/Autonomous/Paths/Red/Red1.java
+++ src/main/java/frc/robot/commands/Autonomous/Paths/Red/Red1.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.Autonomous.Paths.Red;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.DriveCommands.*;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class Red1 extends SequentialCommandGroup{
@@ -11,14 +12,21 @@ public class Red1 extends SequentialCommandGroup{
      */
     public Red1(DriveSubsystem m_drive) {
         addCommands(
+      
         /**
          * "robot will start with claw backwards"
          * extend arm towards node 3 
          * drop cone (open claw)
          * retract arm
          * drive forwarard towards the middle 
+         */ 
+         new DriveDistance(m_drive, 210, 0.5),
+         /**
          * align robot  with object 1 
          * move robot fowarard 
+         */
+        new DriveDistance(m_drive, 14, 0.5),
+        /**
          * turn arm towards object 1 
          * extend arm and lower arm
          * close claw on object 1 
@@ -27,10 +35,14 @@ public class Red1 extends SequentialCommandGroup{
          *(1) if end on charge station 
          *      turn toward charge station
          *      move robot foward
-         *      balence on charge station
+         */ 
+         new DriveDistance(m_drive,114 ,0.5),
+         /**     balence on charge station
          * (2) if head to  community
          *      trun robot 180 degress 
-         *       move robot foward
+         */
+        new DriveDistance(m_drive, 224, 50)
+        /**
          *      stop near node 3 or 2 
          *      lower arm to avalible node 
          *      extend arm toward availble node 
--- src/main/java/frc/robot/commands/Autonomous/Paths/Red/Red3.java
+++ src/main/java/frc/robot/commands/Autonomous/Paths/Red/Red3.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.Autonomous.Paths.Red;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.DriveCommands.DriveDistance;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class Red3 extends SequentialCommandGroup{
@@ -8,19 +9,26 @@ public class Red3 extends SequentialCommandGroup{
 
     public Red3(DriveSubsystem m_drive) {
         addCommands(
-    /**
-     * (arm starts backwards)extend arm toward node 8 or 9
-     * open claw 
-     * drive foward while retracting arm a little 
-     * turn turret toward object 4
-     * lower and extend arm toward object 4
-     * retract arm
-     * option(1)tuen around and head to coummunity node 8 or 9
-     *  extend arm toward node 
-     *  open claw 
-     * option (2) turn toward chargeing station
-     * go onto charge station and balence 
-     */
+            /** (make sure code is in opisite deriection as red)
+             * (arm starts backwards)extend arm toward node 8 or 9
+             * open claw 
+             * drive foward while retracting arm a little 
+             */
+            new DriveDistance(m_drive, 210, 0.5),
+            /**
+             * turn turret toward object 4
+             * lower and extend arm toward object 4
+             * retract arm
+             * option(1)turn around and head to coummunity node 8 or 9
+             */ new DriveDistance(m_drive, 210, 0.5),
+            /**
+             *  extend arm toward node 
+             *  open claw 
+             * option (2) turn toward charging station
+             * go onto charge station
+             */ new DriveDistance(m_drive, 114, 0.5)
+            /**  balence 
+             */
         );
     }
 
