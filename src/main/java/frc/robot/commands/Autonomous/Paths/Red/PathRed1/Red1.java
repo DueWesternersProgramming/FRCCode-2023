5d8305b10a9a87be639409e7d6ba1f6186732f99
DueWesternersProgramming <72952036+DueWesternersProgramming@users.noreply.github.com>
2023-03-09 22:14:49 -0500

    2 WORDS

--- src/main/java/frc/robot/commands/Autonomous/Paths/Red/PathRed1/Red1.java
+++ src/main/java/frc/robot/commands/Autonomous/Paths/Red/PathRed1/Red1.java
@@ -41,37 +41,41 @@ public class Red1 extends SequentialCommandGroup {
         /**
          * "robot will start with claw backwards"
          * extend arm towards node 3C*/
-        new ClawClose(m_claw, 1),
+        //new ClawClose(m_claw, 1),
         
-        new BaseArmPop(m_armBase),
-        new ArmExtend(m_arm),
+        //new BaseArmPop(m_armBase),
+        //new ArmExtend(m_arm),
         //new TurretTurnAuto(m_turret, (double) 180),
         //  drop cone (open claw),
-        new ClawOpen(m_claw),
-        new WaitCommand(0.5),
+        //new ClawOpen(m_claw),
+        //new WaitCommand(0.5),
         /** retract arm*/
-        new ArmRetract(m_arm),
+        //new ArmRetract(m_arm),
          /** drive forwarard towards the middle 
-         */ 
-        new DriveDistance(m_drive, 210, 0.5),
-
+         */
+        new DriveDistance(m_drive, -15, 0.05),
+        new WaitCommand(1),
+        new DriveDistance(m_drive, 10, 0.05),
+        new DriveDistance(m_drive, 28, 0.07)
          /**
          * align robot  with object 1 
          * move robot fowarard 
          */
-        new TurnDegrees(m_drive, -5, 20, 0, 0),
-        new DriveDistance(m_drive, 14, 0.5),
+        
+         //new TurnDegrees(m_drive, -5, 20, 0, 0),
+        //new DriveDistance(m_drive, 14, 0.5),
        
         /**
          * turn arm towards object 1*/
-        new TurretTurnAuto(m_turret, TurretConstants.k90Degrees), /** turn turret 180*/
+        //new TurretTurnAuto(m_turret, TurretConstants.k90Degrees), /** turn turret 180*/
          /* extend arm and lower arm down*/
-        new ArmScore(m_arm),/** new base manuel move arm down*/
-        new BaseArmExtend(m_armBase),
+        //new ArmScore(m_arm),/** new base manuel move arm down*/
+        //new BaseArmExtend(m_armBase),
          /** close claw on object 1 */
-         new ClawClose(m_claw, 0),
+         //new ClawClose(m_claw, 0),
          /** raise arm*/
-         new BaseArmUp(m_armBase),
+         //new BaseArmUp(m_armBase),
+         
          /* choose either to go to (1) charge station or or(2) head back to community
          *(1) if end on charge station 
          *      turn toward charge station
@@ -86,7 +90,7 @@ public class Red1 extends SequentialCommandGroup {
         // new DriveDistance(m_drive, 224, 0.5),
 
 
-        new ConditionalCommand(new Red1Score(m_drive, m_arm, m_armBase, m_claw, m_turret), new Red1ChargeStation(m_drive, m_arm, m_armBase, m_claw, m_turret), ending)
+        //new ConditionalCommand(new Red1Score(m_drive, m_arm, m_armBase, m_claw, m_turret), new Red1ChargeStation(m_drive, m_arm, m_armBase, m_claw, m_turret), ending)
         /**
          *      stop near node 3 or 2 
          *      lower arm to avalible node 
